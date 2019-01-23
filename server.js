3eb24a301a7cdc6e121047aa1863765153630f96
Anwana Ntofon (Art account) <antofon@art.com>
2019-01-23 15:46:54 -0800

    Refactor to create index files holding ART and APC Heroes and SubHeroes in Galleries folder

--- server.js
+++ server.js
@@ -1,62 +1,77 @@
 var path = require('path'),
   fs = require('fs');
 
-// Input Folder
-const projectFolder = './PROJECTS/ART/Heroes/';
-const projects = [];
 
-fs.readdir(projectFolder, (err, files) => {
-  files.forEach(file => {
-    console.log(fs.statSync(projectFolder + file).isDirectory());
-    // Making sure its a directory
-    if (fs.statSync(projectFolder + file).isDirectory()) {
-      projects.push(projectFolder + file + '/index.html');
-    }
-  });
+//create array holding the path for APC Heroes and Subheroes, same for ART
+let projectPaths = [
+  'PROJECTS/ART/Heroes/',
+  'PROJECTS/ART/SubHeroes/',
+  'PROJECTS/APC/Heroes/',
+  'PROJECTS/APC/SubHeroes/'
+];
 
-  processFile();
-})
+//create function to loop through array, passing path as parameter to construct the HTML
+projectPaths.forEach(path => {
+  console.log(path);
 
-function processFile() {
-  console.log(projects);
-  var header = '<head> \
+  // Input Folder
+  const projectFolder = path;
+  const projects = [];
+
+  fs.readdir(projectFolder, (err, files) => {
+    files.forEach(file => {
+      console.log(fs.statSync(projectFolder + file).isDirectory());
+      // Making sure its a directory
+      if (fs.statSync(projectFolder + file).isDirectory()) {
+        projects.push(projectFolder + file + '/index.html');
+      }
+    });
+
+    processFile();
+  })
+
+  function processFile() {
+    console.log(projects);
+    var header = '<head> \
       <meta name="viewport" content="width=device-width, initial-scale=1.0"> \
-      <link rel="stylesheet" href="./styles.css"> \
+      <link rel="stylesheet" href="../../styles.css"> \
       </head>';
 
-  var content = "<body>";
+    var content = "<body>";
 
-  // Ordering with most current on top
-  var sortedProjects = projects.reverse();
-  sortedProjects.forEach(function (path) {
+    // Ordering with most current on top
+    var sortedProjects = projects.reverse();
+    sortedProjects.forEach(function (path) {
 
-    // Formatting the Date/Title displayed
-    var fragLength = path.split("/");
-    var frag = fragLength[fragLength.length - 2].split("_");
-    console.log(frag);
-    var title = '';
+      // Formatting the Date/Title displayed
+      var fragLength = path.split("/");
+      var frag = fragLength[fragLength.length - 2].split("_");
+      console.log(frag);
+      var title = '';
 
-    // Separates the title by the underscores
-    for (var i = 1; i < frag.length; i++) {
-      title += frag[i] + ' ';
-    }
+      // Separates the title by the underscores
+      for (var i = 1; i < frag.length; i++) {
+        title += frag[i] + ' ';
+      }
 
-    var formattedDate = frag[0].substring(4, 6) + '/' + frag[0].substring(6, 8) + '/' + frag[0].substring(0, 4);
+      var formattedDate = frag[0].substring(4, 6) + '/' + frag[0].substring(6, 8) + '/' + frag[0].substring(0, 4);
 
-    // Writing each individual Hero Unit
-    content += '<div class="hero-unit"> \
+      // Writing each individual Hero Unit
+      content += '<div class="hero-unit"> \
         <h1>' + formattedDate + ' ' + title + '</h1> \
-        <iframe src="../' + path + '" width="100%" height="667px"></iframe> \
+        <iframe src="../../../' + path + '" width="100%" height="667px"></iframe> \
         </div>';
-  });
+    });
+    content += "</body>";
 
-  content += "</body>";
+    var fileName = 'index.html';
+    //truncate 'PROJECTS/' prefix ---> becomes directory paths for ART and APC Heroes and Subheroes
+    var projectFragPath = path.replace("PROJECTS/", "");
+    var stream = fs.createWriteStream('./GALLERIES/' + projectFragPath + fileName);
+    stream.once('open', function (fd) {
+      var html = header + content;
 
-  var fileName = 'index.html';
-  var stream = fs.createWriteStream('./GALLERIES/' + fileName);
-  stream.once('open', function (fd) {
-    var html = header + content;
-
-    stream.end(html);
-  });
-}
+      stream.end(html);
+    });
+  }
+});
